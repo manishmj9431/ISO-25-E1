bdd77f51bcf8b2c653f6b50bed60ad7c837a4e61
Manish <manishmj9431@gmail.com>
2020-02-23 01:15:47 +0530

    initial commit

---
+++ isohack/urls.py
@@ -0,0 +1,31 @@
+"""isohack URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from myapp import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('index', views.index,name="index"),
+    path('', views.after_login,name="dashboard"),
+    path('login_page/', views.login_page,name="login_page"),
+    path('user_login/', views.user_login,name="user_login"),   
+    path('user_logout/', views.user_logout,name="user_logout"),   
+    path('teacher_home/', views.teacher_home,name="teacher_home"),   
+    path('student_home/', views.student_home,name="student_home"),   
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)   
---
+++ myapp/views.py
@@ -0,0 +1,54 @@
+from django.shortcuts import render,redirect
+from django.contrib.auth import authenticate,login,logout
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseRedirect, HttpResponse
+
+# Create your views here.
+def index(request):
+    return render(request,'index.html',{})
+
+def user_login(request):
+
+    if request.method == 'POST':
+
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(username=username,password=password)
+        if user:
+            if user.is_active:
+                login(request,user)
+                val = "student"
+                # request.session['member_cat'] = val
+                # return HttpResponseRedirect(reverse('index'))
+                if val == 'student':
+                    return redirect('student_home')
+                else:
+                    return redirect('teacher_home')
+        else:
+            return render(request,'index.html',{})
+              
+    return render(request,'index.html',{})
+
+@login_required(login_url='index')
+def user_logout(request):
+    logout(request)
+    return HttpResponseRedirect(reverse("index"))
+
+def teacher_home(request):
+    return render(request,'teacher_home.html')
+
+def student_home(request):
+    return render(request,'student_home.html')
+
+@login_required(login_url='index')
+def after_login(request):
+    val = Profile.objects.get(user=request.user).category
+    if val == 'student':
+        return redirect('student_home')
+        # return redirect('apply_project')
+    else:
+        return redirect('teacher_home')
+
+def login_page(request):
+    return render(request,'login.html')
